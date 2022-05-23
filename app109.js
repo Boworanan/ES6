243766e59a3f1a19168b4d9ee3a46720f97ec5bc
Boworanan <bowonranan.30@gmail.com>
2022-05-23 17:15:45 +0700

    ES6

---
+++ app109.js
@@ -0,0 +1,16 @@
+//equql operator
+console.log(2 == 2)//true
+console.log(2 == '2')//false
+
+//n equal operator 
+console.log(5!=4)//false
+console.log('hello' != 'Hello')//true
+
+//strict equal operator 
+console.log(2 === 2)//true
+console.log(2 === '2')//false
+
+//strict not equal operator 
+console.log(2 !== 2)//true
+console.log(2 !== '2')//false
+
