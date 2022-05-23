f738b2d7a5fc44fc70da557110ff713232253bbf
Boworanan <bowonranan.30@gmail.com>
2022-05-23 16:52:02 +0700

    ES6

---
+++ app107.js
@@ -0,0 +1,24 @@
+let x = 5;
+let y = 3;
+
+//addition
+console.log(`x + y = ${x + y}`)
+
+//subtraction
+console.log(`x - y = ${x - y}`)
+
+//muliplication
+console.log(`x * y = ${x * y}`)
+
+//remainder
+console.log(`x % y = ${x % y}`)
+
+//increment
+console.log(`++x = ${++x}`)
+console.log(`x++ = ${x++}`)
+console.log(`x = ${x}`)
+
+//drecrement
+console.log(`--y = ${--y}`)
+console.log(`y-- = ${y--}`)
+console.log(`y = ${y}`)
---
+++ app108.js
@@ -0,0 +1,18 @@
+//Assignment[=]
+let x = 20
+//console.log(x)//20
+
+//add Assignment[+=]
+let y = 20;
+y += 10 //y = y + 10
+//console.log(y)
+
+//sub Assignment[+=]
+let z = 20;
+z -= 10 //z = z - 10
+//console.log(z)
+
+//multi Assignment[*=]
+let a = 20;
+a *= 10 //a = a * 10
+console.log(a)
