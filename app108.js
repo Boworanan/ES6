9d6b16ec1db8bf14545d78df5a2a8d9ce33ab8c5
Boworanan <bowonranan.30@gmail.com>
2022-05-23 16:58:53 +0700

    ES6

--- app108.js
+++ app108.js
@@ -15,4 +15,14 @@ z -= 10 //z = z - 10
 //multi Assignment[*=]
 let a = 20;
 a *= 10 //a = a * 10
-console.log(a)
+//console.log(a)
+
+//Remainder Assignment[%=]
+let b = 20;
+b %= 10 //b = b % 10
+//console.log(b)
+
+//exponentiation Assignment[**=]
+let c = 10;
+c **= 3 //c = c ** 3
+console.log(c)
