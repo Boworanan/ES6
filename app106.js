dcecfb8c55af9e05729343448977b7705e5b0679
Boworanan <bowonranan.30@gmail.com>
2022-05-23 16:14:53 +0700

    ES6

---
+++ app106.js
@@ -0,0 +1,6 @@
+let info = "ผวจ. กทม. ผู้มีคะแนนสูงสุดในประวัติศาสตร์";
+let fname = "ชัชชาติ สิทธิพันธ์";
+let age = 50;
+let isTrue = true;
+
+console.log(`${info} Your name is ${fname} Your age is ${age} is ${isTrue}`);
